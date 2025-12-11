5145d5d17f7f980f4ccf1750ded4177d01dd1bca
Shahabuddin1122 <shavoddin54@gmail.com>
2025-12-11 07:37:40 +0600

    add database configuration

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,10 +3,17 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const getDatabaseConfig = (): TypeOrmModuleOptions => ({
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: 5432,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || '1234',
   database: process.env.DB_DATABASE || 'water_tariff',
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
+  ssl: process.env.DB_HOST?.includes('render.com')
+    ? { rejectUnauthorized: false }
+    : false,
+  extra: {
+    connectionTimeoutMillis: 10000,
+    query_timeout: 10000,
+  },
 });
